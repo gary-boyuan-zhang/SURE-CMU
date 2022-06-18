9a8fb71a8a9ad17c7581156e944578d05a74832d
gary-boyuan-zhang <bz2058@nyu.edu>
2022-06-18 01:04:36 -0400

    06/18 update

--- EDA - NHL/team.R
+++ EDA - NHL/team.R
@@ -81,3 +81,111 @@ team_ongoal <- team_data %>%
   theme(legend.position = 'bottom')
 
 team_shot + team_ongoal + team_score
+
+
+
+
+# hcluster ----------------------------------------------------------------
+
+  # shot_pg vs. shot_con_pg
+
+    ## construct team_shot_dist & team_shot_dist_matrix
+
+team_shot_dist <- team_data %>%
+  select(shot_pg, shot_con_pg) %>%
+  dist()
+
+team_shot_dist_matrix <- team_shot_dist %>%
+  as.matrix()
+
+rownames(team_shot_dist_matrix) <- team_data$teamCode
+colnames(team_shot_dist_matrix) <- team_data$teamCode
+
+head(team_shot_dist_matrix)
+
+    ## pivot longer to get team_shot_dist_table
+
+team_shot_dist_table <- team_shot_dist_matrix %>%
+  as_tibble() %>%
+  mutate(team1 = rownames(team_shot_dist_matrix)) %>%
+  pivot_longer(cols = -team1,
+               names_to = 'team2',
+               values_to = 'dist')
+
+    ## plot unordered distance heatmap
+
+team_shot_dist_table %>%
+  ggplot(aes(x = team1, y = team2,
+             fill = dist)) +
+  geom_tile() +
+  theme_bw() +
+  theme(legend.position = 'bottom') +
+  scale_fill_gradient(low = 'darkorange',
+                      high = 'darkblue')
+
+    ## order dist table with serialization
+
+order <- team_shot_dist %>%
+  seriate() %>%
+  get_order()
+
+team_shot_order <- team_data$teamCode[order]
+
+    ## plot ordered distance heatmap
+
+team_shot_dist_table %>%
+  mutate(team1 = fct_relevel(team1, team_shot_order),
+         team2 = fct_relevel(team2, team_shot_order)) %>%
+  ggplot(aes(x = team1, y = team2,
+             fill = dist)) +
+  geom_tile() +
+  theme_bw() +
+  theme(legend.position = 'bottom') +
+  scale_fill_gradient(low = 'darkorange',
+                      high = 'darkblue')
+
+    ## h cluster with minimax linkage
+
+library(protoclust)
+
+team_shot_minimax <- protoclust(team_shot_dist)
+
+    ## protoclust dendrogram
+
+library(ggdendro)
+
+team_shot_dendro <- team_shot_minimax %>%
+  ggdendrogram(theme_dendro = FALSE,
+               labels = FALSE,
+               leaf_labels = FALSE) +
+  geom_hline(yintercept = 6, linetype = 'twodash', color = 'red') +
+  labs(y = 'Maximum dissimilarity from prototype') +
+  theme_bw() +
+  theme(axis.text.x = element_blank(),
+        axis.title.x = element_blank(),
+        axis.ticks.x = element_blank(),
+        panel.grid = element_blank())
+
+    ## protocut
+
+team_shot_minimax_cluster <- team_shot_minimax %>%
+  protocut(h = 6)
+
+    ## plot protoclust result
+
+team_shot_clustered <- team_data %>%
+  mutate(team_clusters = as.factor(team_shot_minimax_cluster$cl)) %>%
+  ggplot(aes(x = shot_pg, y = shot_con_pg,
+             color = team_clusters,
+             label = teamCode,
+             #size = abs(shot_pg - shot_con_pg)
+             )) +
+  geom_point() + 
+  geom_text(hjust = 0.8, vjust = 1.2) +
+  geom_abline(slope = 1, intercept = 0) +
+  scale_color_colorblind() +
+  theme_bw() +
+  theme(legend.position = 'bottom')
+
+team_shot_dendro + team_shot_clustered
+
